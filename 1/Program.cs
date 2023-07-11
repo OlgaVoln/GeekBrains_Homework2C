c9783f49c064134c8739277e00963b683826af2b
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-11 12:05:54 +0300

    Задача 1 решена

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//  Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число число");
+int number = Convert.ToInt32(Console.ReadLine());
+int Digit = number % 100;
+int secondDigit = Digit / 10;
+Console.WriteLine($"{number} -> {secondDigit}");
